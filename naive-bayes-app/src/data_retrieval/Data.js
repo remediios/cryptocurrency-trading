8822028e7f65e0b3241a1159f7c875d1a4518aa8
Miguel Remedios <miguelremediios@gmail.com>
2023-06-07 17:24:54 +0100

    BTC updates

--- naive-bayes-app/src/data_retrieval/Data.js
+++ naive-bayes-app/src/data_retrieval/Data.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Table, Pagination } from "antd";
 
 function Data() {
   const [bitcoinData, setBitcoinData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const rowsPerPage = 10;
 
   useEffect(() => {
     async function getBitcoinHistoricalData() {
@@ -13,7 +16,7 @@ function Data() {
           {
             params: {
               vs_currency: "gbp",
-              days: "90", // Retrieve data for the last 30 days
+              days: "90", // Retrieve data for the last 90 days
             },
           }
         );
@@ -21,9 +24,10 @@ function Data() {
         const { prices } = response.data;
         // Extract the timestamps and prices from the nested array
         const formattedData = prices.map(([timestamp, price]) => ({
-          date: new Date(timestamp).toLocaleDateString(),
+          date: new Date(timestamp).toLocaleString(),
           price,
         }));
+
         // Store the formatted data in the state array
         setBitcoinData(formattedData);
         setIsLoading(false);
@@ -35,26 +39,51 @@ function Data() {
     getBitcoinHistoricalData();
   }, []);
 
+  // Calculate the index of the last row to display based on the current page and rows per page
+  const lastIndex = currentPage * rowsPerPage;
+  // Calculate the index of the first row to display
+  const firstIndex = lastIndex - rowsPerPage;
+  // Slice the bitcoinData array to get the rows for the current page
+  const currentRows = bitcoinData.slice(firstIndex, lastIndex);
+
+  // Handle page change
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // Define the columns for the table
+  const columns = [
+    {
+      title: "Date",
+      dataIndex: "date",
+      key: "date",
+    },
+    {
+      title: "Price (USD)",
+      dataIndex: "price",
+      key: "price",
+    },
+  ];
+
   return (
     <div>
       <h1>Bitcoin Historical Data</h1>
       {isLoading ? (
         <p>Loading Bitcoin data...</p>
       ) : bitcoinData.length > 0 ? (
-        <table>
-          <tbody>
-            <tr>
-              <td>Date</td>
-              <td>Price (USD)</td>
-            </tr>
-            {bitcoinData.map((dataPoint, index) => (
-              <tr key={index}>
-                <td>{dataPoint.date}</td>
-                <td>{dataPoint.price}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <>
+          <Table
+            columns={columns}
+            dataSource={currentRows}
+            pagination={false}
+          />
+          <Pagination
+            current={currentPage}
+            total={bitcoinData.length}
+            pageSize={rowsPerPage}
+            onChange={handlePageChange}
+          />
+        </>
       ) : (
         <p>No Bitcoin data available.</p>
       )}
